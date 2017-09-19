586cacbbd383bf8434d449d01f141b01f124f0ad
ambhoyland <ambhoyland@gmail.com>
2017-09-19 14:53:19 +0000

    Changed date format, added code to allow for video creation in python

--- static_map.py
+++ static_map.py
@@ -1,77 +1,79 @@
 """produces static maps using mplleaflet which can be stitched together to produce a gif...vid???"""
 
-import mplleaflet
 import matplotlib.pyplot as plt
+import pandas as pd
+from datetime import timedelta, date
+from datetime import datetime
+import datetime as dt
 import numpy as np
 import glob
 from PIL import Image
 from PIL import ImageFont
 from PIL import ImageDraw
 from selenium import webdriver
+# import cv2
 import os
-import pandas as pd
 import time
+import mplleaflet
 
-blackpool_df = pd.read_csv("BlackpoolMOD3.csv")
-print(blackpool_df.head())
-print(len(blackpool_df))
+#reads in file
+blackpool_df = pd.read_csv("BlackpoolMOD.csv")
 
-for row in blackpool_df['Visit Outcome']:
+#converts date to english format and a datetime format rather than string
+blackpool_df['Date_of_Visit'] = pd.to_datetime(blackpool_df.Date_of_Visit, dayfirst = True)
 
-    temp_df = blackpool_df.loc[blackpool_df['Visit Outcome'] == 'No response']
+#adds a column with day of year in it.
+blackpool_df['Day_of_Year'] = blackpool_df.Date_of_Visit.dt.dayofyear
 
-print(len(temp_df))
+#reports the earliest day in the year of the sample.
+minday = blackpool_df.Date_of_Visit.dt.dayofyear.min()
 
-# creating lists of data
-lat = blackpool_df['LATITUDE'].tolist()
-long = blackpool_df['LONGITUDE'].tolist()
-day = blackpool_df['Day'].tolist()
-response = blackpool_df['Visit Outcome'].tolist()
-rs_list = len(day)
+#takes the day in the year, minus the mininmum day of the year to return the number of day in the range
+blackpool_df['Day_of_Range'] = blackpool_df.Day_of_Year - minday
 
+#print (blackpool_df.head())
 
-# Attempting to define non-responses for purposes of separation
-
-def non_response(response):
-    for idx, val in enumerate(response):
-        if val.lower() == 'no response':
-            day[idx] += range(max(27) - day[idx])
-
-non_response(response)
-
-# define some functions...
-def plot_data(i, input_list, list_length):
-
-    subset_len = len([entry for entry in day if entry >= i])
-    subset_pos = list_length - subset_len
-    nr_coord_list = input_list[subset_pos:]
-    coord_list = input_list[:subset_pos]
-    return nr_coord_list, coord_list
+#nr2_records = blackpool_df[(blackpool_df["Visit_Outcome"] == 'No response') & (blackpool_df["Day_of_Range"] == 2)]
+#print (nr2_records)
 
 
-for date in range(max(day)):
+for i in range(0,blackpool_df["Day_of_Range"].max()+1):
 
-    nr_lats, lats = plot_data(date, lat, rs_list)
-    nr_longs, longs = plot_data(date, long, rs_list)
+# (blackpool_df["Visit_Outcome"] == 'No response') & (blackpool_df["Day_of_Range"] <= 27)
 
-    plt.plot(longs, lats, 'gs', markersize=1)  # Draws green squares
-    plt.plot(nr_longs, nr_lats, 'rs', markersize=1)   # Draws red squares
+        plt.plot(blackpool_df["LONGITUDE"],blackpool_df["LATITUDE"],'rs',markersize=1)
 
-    mplleaflet.save_html(fileobj=str(date)+".html")
-    # saves as html file
+        r_records = blackpool_df[(blackpool_df["Visit_Outcome"] == 'Response') & (blackpool_df["Day_of_Range"] <= i)]
 
+        plt.plot(r_records["LONGITUDE"],r_records["LATITUDE"],'gs',markersize=1)
+
+
+     #   blackpool_df[(blackpool_df["Visit_Outcome"] == 'Response') & (blackpool_df["Day_of_Range"] <= i)]
+
+      #  plt.plot(blackpool_df["LONGITUDE"],blackpool_df["LATITUDE"],'gs',markersize=2)
+
+        mplleaflet.save_html(fileobj=str(i)+".html")
 
 # get a list of all the files to open
 glob_folder = os.path.join(os.getcwd(), '*.html')
 
 html_file_list = glob.glob(glob_folder)
 
+start_date = dt.date(*map(int,"2017,4,13".split(',')))
+print(start_date)
+
+
 for html_file in html_file_list:
 
     # get the name into the right format
     temp_name = "file://" + html_file
+    #print(len(temp_name))
 
     index = int(temp_name[58:-5])
+    print(index)
+    temp_date = start_date + dt.timedelta(days=index)
+
+
 
     # open in webpage
     driver = webdriver.Firefox()
@@ -86,7 +88,7 @@ for html_file in html_file_list:
 
     fontPath = "/usr/share/fonts/truetype/dejavu/DejaVuSans.ttf"
     font = ImageFont.truetype(fontPath, 32)
-    draw.text((310, 110), "Day " + str(index), (0, 0, 0), font=font)
+    draw.text((310, 110), "Day " + str(index) + ' \n' + str(temp_date.strftime('%d-%m-%Y')), (0, 0, 0), font=font)
     img.save(save_name)
 
     # crop as required
@@ -94,8 +96,23 @@ for html_file in html_file_list:
     area = img.crop(box)
 
     if index >= 10:
-        area.save('0' + str(index), 'png')
+        area.save('0' + str(index) + '.png')
     else:
-        area.save('00' + str(index), 'png')
+        area.save('00' + str(index) + '.png')
 
+"""
+image_folder = 'home/bigdata/PycharmProjects/VisualisationProject/images'
+video_name = 'python_video_test.mp4'
 
+images = [img for img in os.listdir(image_folder) if img.endswith(".png")]
+frame = cv2.imread(os.path.join(image_folder, images[0]))
+height, width, layers = frame.shape
+
+video = cv2.VideoWriter(video_name, -1, 1, (width,height))
+
+for image in images:
+    video.write(cv2.imread(os.path.join(image_folder, image)))
+
+cv2.destroyAllWindows()
+video.release()
+"""
